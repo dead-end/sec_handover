7a65e0e66f0f295486989d3cace3cb7cdfd68411
dead end <dead.end@inbox.ru>
2017-08-27 11:16:33 +0200

    initialversion

---
+++ include/sh_utils.h
@@ -0,0 +1,27 @@
+/*
+ * sh_utils.h
+ *
+ *  Created on: Aug 14, 2017
+ *      Author: dead-end
+ */
+
+#ifndef SH_UTILS_H_
+#define SH_UTILS_H_
+
+void print_block(const char *msg, const unsigned char *block, const int block_size, const int per_line);
+
+bool write_array(FILE *file, const unsigned char *array, const size_t array_len);
+
+bool write_array_to(FILE *file, const unsigned char *array, const size_t array_len, const long offset, const int whence);
+
+bool read_array(FILE *file, unsigned char *array, const size_t array_len);
+
+bool read_array_from(FILE *file, unsigned char *array, const size_t array_len, const long offset, const int whence);
+
+void fclose_silent(FILE *file, const char *file_name);
+
+bool compare_files(const char *file_name_1, const char *file_name_2);
+
+bool get_file_size(const int fd, size_t *ptr);
+
+#endif /* SH_UTILS_H_ */
