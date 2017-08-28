c51cd8925b9fa8d48342268bbb623348567cc318
dead end <dead.end@inbox.ru>
2017-08-28 21:55:13 +0200

    format and comments

--- src/sh_generate_keys.c
+++ src/sh_generate_keys.c
@@ -6,27 +6,35 @@
  */
 
 #include <stdbool.h>
-#include <stdlib.h>
-#include <gcrypt.h>
 #include <errno.h>
+
+#include <gcrypt.h>
 #include <time.h>
-#include "../include/sh_commons.h"
-#include "../include/sh_keys.h"
+
+#include "sh_commons.h"
+#include "sh_keys.h"
 
 #define BLOCK_SIZE 16
 
 /***************************************************************************
- *
+ * The function writes the content of a c-array with random bytes to a given
+ * file. It adds a newline after BLOCK_SIZE bytes.
  **************************************************************************/
 
-static bool print_random_bytes(FILE *file, size_t num_bytes) {
+static bool print_random_bytes(FILE *file, const size_t num_bytes) {
 	int modulo;
 
-	if (num_bytes % 16 != 0) {
+	//
+	// ensure that the number of bytes is a multiple of the block size
+	//
+	if (num_bytes % BLOCK_SIZE != 0) {
 		print_error("print_random_bytes() Invalid size %zu\n", num_bytes);
 		return false;
 	}
 
+	//
+	// we use gcrypt to generate random bytes
+	//
 	unsigned char *random = gcry_random_bytes_secure(num_bytes, GCRY_STRONG_RANDOM);
 
 	for (int i = 0; i < num_bytes; i++) {
@@ -62,11 +70,12 @@ static bool print_random_bytes(FILE *file, size_t num_bytes) {
 }
 
 /***************************************************************************
- *
+ * The function writes the source file with the two keys.
  **************************************************************************/
 
-void genereate_keys_file(const char *file_name, const size_t cipher_key_len, const size_t hmac_key_len) {
+static void genereate_keys_file(const char *file_name, const size_t cipher_key_len, const size_t hmac_key_len) {
 	FILE *file;
+	time_t rawtime;
 
 	file = fopen(file_name, "w+");
 	if (file == NULL) {
@@ -74,27 +83,41 @@ void genereate_keys_file(const char *file_name, const size_t cipher_key_len, con
 		exit(EXIT_FAILURE);
 	}
 
-	time_t rawtime;
-
+	//
+	// print comment with the generation time
+	//
 	time(&rawtime);
 	fprintf(file, "/**\n * KEYS GENERATED AT: %s */\n\n", ctime(&rawtime));
 
+	//
+	// cipher key
+	//
 	fprintf(file, "unsigned char cipher_key[%zu] = {\n", cipher_key_len);
 	print_random_bytes(file, cipher_key_len);
 	fprintf(file, "};\n\n");
 
+	//
+	// hmac key
+	//
 	fprintf(file, "unsigned char hmac_key[%zu] = {\n", hmac_key_len);
 	print_random_bytes(file, hmac_key_len);
 	fprintf(file, "};\n");
-
 }
 
-
 /***************************************************************************
- * The main function simply triggers the tests.
+ * The main function triggers the generation process of the source file with
+ * the two keys. It is called with the name of the c file, that should be
+ * created.
  **************************************************************************/
 
 int main(const int argc, const char *argv[]) {
-	genereate_keys_file("src/sh_generated_keys.c", CIPHER_KEY_LEN, HMAC_KEY_LEN);
+
+	if (argc != 2) {
+		print_error_str("main() Usage: sh_generate_keys <sh_generated_keys.c>");
+		return EXIT_FAILURE;
+	}
+
+	genereate_keys_file(argv[1], CIPHER_KEY_LEN, HMAC_KEY_LEN);
+
 	return EXIT_SUCCESS;
 }
